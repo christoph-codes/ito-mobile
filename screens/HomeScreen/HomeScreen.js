238f06c470cf27b7b24c955e8f5f73d853b1710c
Christopher Jones <tkcwebdev@gmail.com>
2021-10-26 01:19:58 -0700

    init: Updated home screen to use correct flex styles. Added correct firebase querys to find family. Used new logo component variant. Added Copyright to this page instead of the container component. May move again to marketing frame once one is created.

--- screens/HomeScreen/HomeScreen.js
+++ screens/HomeScreen/HomeScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import InputText from '../../components/InputText';
 // import InputRadio from '../../components/InputRadio';
 import Button from '../../components/Button';
@@ -8,12 +8,27 @@ import FeedbackText from '../../components/FeedbackText/FeedbackText';
 import H1 from '../../components/H1';
 import Logo from '../../components/Logo';
 import { firestore } from '../../config/firebaseConfig';
+import colors from '../../GlobalStyles';
 
 const styles = StyleSheet.create({
+	centerView: {
+		flex: 1,
+		justifyContent: 'center',
+		width: '100%',
+		maxWidth: 400,
+		alignSelf: 'center',
+	},
 	radios: {
 		flexDirection: 'row',
 		justifyContent: 'space-between',
 		marginVertical: 16,
+	},
+	copyright: {
+		backgroundColor: colors.primary,
+		textAlign: 'center',
+		paddingBottom: 32,
+		justifyContent: 'flex-end',
+		color: colors['primary-light'],
 	},
 });
 
@@ -27,21 +42,21 @@ const HomeScreen = ({ navigation }) => {
 				.collection('users')
 				.where('familycode', '==', familyCode);
 			users.get().then((snapshot) => {
-				console.log(snapshot.docs);
-				// if (!snapshot.empty) {
-				// 	snapshot.docs.forEach((doc) => {
-				// 		// console.log('docdata', doc.data());
-				// 		const parent = doc.data();
-				// 		navigation.navigate('ChildLogin', parent);
-				// 		// if (userType === 'parent') {
-				// 		// 	history.push('/parent-login', parent.email);
-				// 		// } else {
-				// 		// 	history.push('/child-login', parent);
-				// 		// }
-				// 	});
-				// } else {
-				// 	setFeedback('This is not a valid family code.');
-				// }
+				// console.log('snapshot:', snapshot.docs);
+				if (!snapshot.empty) {
+					snapshot.docs.forEach((doc) => {
+						setFeedback('');
+						const parent = doc.data();
+						navigation.navigate('ChildLogin', parent);
+						// if (userType === 'parent') {
+						// 	history.push('/parent-login', parent.email);
+						// } else {
+						// 	history.push('/child-login', parent);
+						// }
+					});
+				} else {
+					setFeedback('This is not a valid family code.');
+				}
 			});
 
 			// if (userType) {
@@ -66,19 +81,20 @@ const HomeScreen = ({ navigation }) => {
 		}
 	};
 	return (
-		<Container>
-			<View style={styles.container}>
-				<Logo />
-				<H1>Login</H1>
-				<InputText
-					label="Family Code"
-					autoCompleteType="username"
-					placeholder="jones5"
-					onChangeText={setFamilyCode}
-					value={familyCode.toLowerCase()}
-					onFocus={() => setFeedback('')}
-				/>
-				{/* <View style={styles.radios}>
+		<>
+			<Container>
+				<View style={styles.centerView}>
+					<Logo variant="light" />
+					<H1>Login</H1>
+					<InputText
+						label="Family Code"
+						autoCompleteType="username"
+						placeholder="jones5"
+						onChangeText={setFamilyCode}
+						value={familyCode.toLowerCase()}
+						onFocus={() => setFeedback('')}
+					/>
+					{/* <View style={styles.radios}>
 					<InputRadio
 						label="Parent"
 						className="mr-3"
@@ -95,10 +111,15 @@ const HomeScreen = ({ navigation }) => {
 						onChange={() => setUserType('child')}
 					/>
 				</View> */}
-				<FeedbackText feedback={feedback}>{feedback}</FeedbackText>
-				<Button onPress={login}>Login</Button>
-			</View>
-		</Container>
+					<FeedbackText feedback={feedback}>{feedback}</FeedbackText>
+					<Button onPress={login}>Login</Button>
+				</View>
+			</Container>
+			<Text style={styles.copyright}>
+				Copyright {new Date().getFullYear()} &#169; InThisOrder. All
+				Rights Reserved.
+			</Text>
+		</>
 	);
 };
 
