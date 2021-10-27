7e9fe7a877d51cfd4b65514ddf1fd7beb669f39c
Christopher Jones <tkcwebdev@gmail.com>
2021-10-26 22:58:46 -0700

    init: Install native base component library

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NativeBaseProvider } from 'native-base';
 import HomeScreen from './screens/HomeScreen';
 import LogoTitle from './components/LogoTitle';
 import colors from './GlobalStyles';
@@ -23,36 +24,41 @@ const styles = StyleSheet.create({
 const App = () => {
 	return (
 		// <UserProvider>
-		<View style={styles.App}>
-			<NavigationContainer>
-				<Stack.Navigator
-					screenOptions={{
-						headerTitle: (props) => <LogoTitle {...props} />,
-						headerTintColor: colors.white,
-						headerStyle: {
-							backgroundColor: colors.primary,
-							paddingVertical: 40,
-						},
-					}}
-				>
-					<Stack.Screen
-						options={{ headerShown: false }}
-						name="Login"
-						component={HomeScreen}
-					/>
-					<Stack.Screen
-						options={{ headerShown: false }}
-						name="ChildDashboard"
-						component={ChildDashboard}
-					/>
-					{/* <Stack.Screen
+		<NativeBaseProvider>
+			<View style={styles.App}>
+				<NavigationContainer>
+					<Stack.Navigator
+						screenOptions={{
+							headerTitle: (props) => <LogoTitle {...props} />,
+							headerTintColor: colors.white,
+							headerStyle: {
+								backgroundColor: colors.primary,
+								paddingVertical: 40,
+							},
+						}}
+					>
+						<Stack.Screen
+							options={{ headerShown: false }}
+							name="Login"
+							component={HomeScreen}
+						/>
+						<Stack.Screen
+							options={{ headerShown: false }}
+							name="ChildDashboard"
+							component={ChildDashboard}
+						/>
+						{/* <Stack.Screen
 							name="ParentLogin"
 							component={ParentLogin}
 						/> */}
-					<Stack.Screen name="ChildLogin" component={ChildLogin} />
-				</Stack.Navigator>
-			</NavigationContainer>
-		</View>
+						<Stack.Screen
+							name="ChildLogin"
+							component={ChildLogin}
+						/>
+					</Stack.Navigator>
+				</NavigationContainer>
+			</View>
+		</NativeBaseProvider>
 		// </UserProvider>
 	);
 };
